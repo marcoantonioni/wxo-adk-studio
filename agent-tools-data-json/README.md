cf70a6635b36308ca4221490e55d68dbf786e74e
marcoantonioni <antonioni.marco@gmail.com>
2025-05-29 14:45:04 +0200

    update

--- agent-tools-data-json/README.md
+++ agent-tools-data-json/README.md
@@ -13,9 +13,9 @@ Answers may change based on the LLM model.<br>
 use: watsonx/meta-llama/llama-3-2-90b-vision-instruct<br>
 note: the tool return the name of city in UPPERCASE format
 
-- you say: give the weather forecast for Trieste in date 08/15/2026<br>
+- you say: give the weather forecast for Trieste for date 08/15/2026<br>
 -- agent answer: The weather forecast for Trieste on August 15, 2026, is 26 degrees.
 
-- you say: give the weather forecast for Trieste in date 08/15/2026, you must use city name from tool result<br>
+- you say: give the weather forecast for Trieste for date 08/15/2026, for the answer data you must use the name of city and the date and time in which the request has been issued from tool results<br>
 -- agent answer: The weather forecast for TRIESTE on August 15, 2026, is -4 degrees.
 
--- agent-tools-data-json/weather_forecast.py
+++ agent-tools-data-json/weather_forecast.py
@@ -1,9 +1,17 @@
 from ibm_watsonx_orchestrate.agent_builder.tools import tool
-import json, random
+import json, random, datetime
+
+class WeatherForecast:
+    def __init__(self, city: str, date: str):
+        self.city = city
+        self.date = date
+        self.temperature = random.randint(-10, 40)
+        self.dateOfRequest = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+
 
 @tool( 
     name="MA42021_tool_weather_forecast", 
-    description="This tool accecpt in input the the name of a city and a date, return an json object of type {\"city\": string, \"temperature\": integer}."
+    description="This tool accecpt in input the the name of a city and a date, return an json object with the following data city, forecast date, temperature, date of the request."
     )
 def weatherForecast(city:str, date:str) -> str:
     """
@@ -13,7 +21,6 @@ def weatherForecast(city:str, date:str) -> str:
     :param date: A date in format mm/dd/aaaa.
     :returns: a json object.    
     """
-    temperature: int = random.randint(-10, 40)
-
-    forecast = {"city": city.upper(), "temperature": temperature}
-    return forecast
+    
+    wf = WeatherForecast(city.upper(), date)
+    return json.dumps(wf.__dict__)
