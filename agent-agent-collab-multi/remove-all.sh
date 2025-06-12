2a720ae020eae05d93b99309637187387b9fe970
marcoantonioni <antonioni.marco@gmail.com>
2025-06-12 15:05:47 +0200

    update

--- agent-agent-collab-multi/remove-all.sh
+++ agent-agent-collab-multi/remove-all.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -x
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+${SCRIPT_DIR}/show-agents-local.sh
+
 orchestrate agents remove  -k native -n MA42021_agent_customer_personal_informations
 orchestrate agents remove  -k native -n MA42021_agent_customer_loyalty_informations
 orchestrate agents remove  -k native -n MA42021_agent_customer_transactions_informations
